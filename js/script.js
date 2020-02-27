51644260d2e8e98896c189f3781c1649d39745ec
renan_siqu <renan.siqu@gmail.com>
2020-02-27 12:49:04 -0300

    feat: criado o cenario e nave com movimentação

--- js/script.js
+++ js/script.js
@@ -4,6 +4,30 @@
 
     // recursos do jogo
 
+    // arrays
+    var sprites = []
+    var assetsToLoad = []
+
+    // sprites
+
+    // cenario
+    var background = new Sprite(0,56,400,500,0,0)
+    sprites.push(background)
+
+    // nave
+    var defender = new Sprite(0,0,30,50,185,450)
+    sprites.push(defender)
+
+    // imagem
+    var img = new Image()
+    img.addEventListener("load", loadHandler, false)
+    img.src = "img/img.png"
+
+    assetsToLoad.push(img)
+
+    // contador de recursos
+    var loadedAssets = 0
+
     // entradas
     var LEFT = 37, RIGHT = 39, ENTER = 13, SPACE = 32
 
@@ -15,7 +39,7 @@
     var gameState = LOADING
 
     // listeners
-    window.addEventListener("keydown", function() {
+    window.addEventListener("keydown", function(e) {
         var key = e.keyCode
 
         switch(key) {
@@ -27,7 +51,7 @@
                 break
         }
     },false)
-    window.addEventListener("keyup", function() {
+    window.addEventListener("keyup", function(e) {
         var key = e.keyCode
 
         switch(key) {
@@ -50,18 +74,60 @@
 
     // Funções do jogo
 
+    function loadHandler() {
+        loadedAssets++
+
+        if(loadedAssets === assetsToLoad.length) {
+            img.removeEventListener("load", loadHandler, false)
+
+            // inicia o jogo
+            gameState = PAUSED
+        }
+    }
+
     function loop() {
         requestAnimationFrame(loop, cnv)
-        update()
+
+        switch(gameState) {
+            case LOADING :
+                console.log("Loading...")
+                break
+            case PLAYING :
+                update()
+                break
+        }
+
         render()
     }
 
     function update() {
 
+        // move para a esquerda
+        if(mvLeft && !mvRight) {
+            defender.vx = -5
+        }
+        // move para a direita
+        if(mvRight && !mvLeft) {
+            defender.vx = 5
+        }
+        // para a nave
+        if(!mvLeft && !mvRight) {
+            defender.vx = 0
+        }
+
+        // atualiza a posição
+        defender.x = Math.max(0, Math.min(cnv.width-defender.width, defender.x + defender.vx))
     }
 
     function render() {
-
+        ctx.clearRect(0,0,cnv.width,cnv.height)
+        if(sprites.length !== 0) {
+            for(var i in sprites) {
+                var spr = sprites[i]
+                ctx.drawImage(img, spr.sourceX, spr.sourceY, spr.width, spr.height, Math.floor(spr.x), Math.floor(spr.y), spr.width, spr.height)
+            }
+        }
     }
 
+    loop()
 }())
